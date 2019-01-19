b8879437ec53805e97fedb904f442bb0085fb4a2
ShadyLillith <46405178+ShadyLillith@users.noreply.github.com>
2019-01-19 11:54:57 -0800

    Found Picture

---
+++ JS/type.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    strings: [
+      'I am a Leader',
+      'I am a Student',
+      'I am a Athlete',
+      'I am a Learner',
+      'I am ShadyLillith'
+
+    ],
+    typeSpeed:30,
+    backSpeed:50,
+    backDelay:1500,
+    loop:false
+  });
+});
